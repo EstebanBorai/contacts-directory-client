ba9d758d267017566f92e89333902fbb33398a84
Esteban Borai <estebanborai@outlook.com>
2019-01-31 21:34:47 -0300

    Update Jest config. Coverage: "% Stmts 33.2 - % Branch 19.55 - % Funcs 17.7 - % Lines 33.2"

--- client/jest.config.js
+++ client/jest.config.js
@@ -7,7 +7,16 @@ module.exports = {
     '<rootDir>/__tests__/__mocks__/'
   ],
   moduleNameMapper: {
+    '^api(.*)$': '<rootDir>/src/api$1',
+    '^assets(.*)$': '<rootDir>/src/assets$1',
     '^components(.*)$': '<rootDir>/src/components$1',
-    '\\.(css|less)$': 'identity-obj-proxy'
+    '^contexts(.*)$': '<rootDir>/src/contexts$1',
+    '^helpers(.*)$': '<rootDir>/src/helpers$1'
+  },
+  transform: {
+    '^.+\\.(js|jsx)$': 'babel-jest',
+    '\\.(css|scss)$': '<rootDir>/config/jest/mediaFileTransformer.js',
+    '\\.(bmp|gif|jpg|jpeg|png|psd|svg|webp)$':
+      '<rootDir>/config/jest/mediaFileTransformer.js'
   }
 };
